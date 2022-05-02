db28965b73b1cf9027a4e11ca3cce7bc8b6c8882
anorakich <shevchenko.nv@phystech.edu>
2022-05-02 14:18:19 +0300

    ok lets go

--- miptlance/lib/orderClasses/Order.dart
+++
@@ -1,7 +0,0 @@
-class Order {
-  var data;
-  Order();
-  static Order create() {
-    return Order();
-  }
-}
--- miptlance/lib/subjectsEnum.dart
+++
@@ -1,8 +0,0 @@
-enum subject {
-  calculus,
-  physics,
-  algebra,
-  cpp,
-  algorithms,
-  english,
-  }
